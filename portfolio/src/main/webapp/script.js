988dbdb3a564aa7e3b94b54a5c1f26a6563a7bfb
Shradha <shradhakhapra1899@gmail.com>
2020-07-11 22:44:51 +0530

    Fix indentation, ids & JSDoc param

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,26 +1,26 @@
 /**
   * This is the main javascript file for the portfolio page. Contains a showProjectDetail method.
 
-  Copyright 2019 Google LLC
+  * Copyright 2019 Google LLC
 
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
 
-    https://www.apache.org/licenses/LICENSE-2.0
+  *  https://www.apache.org/licenses/LICENSE-2.0
 
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
 
   * @author: shradha-khapra
   */
 
 /**
   * Shows details of a specified project.
-  * @param {void} 
+  * @param {!Event} event The event object received.
   * @return {void}
   */
 function showProjectDetail(event) {
@@ -35,18 +35,18 @@ function showProjectDetail(event) {
   // Add description to the page. 
   const descriptionDiv = document.createElement('div');
   const targetElement = event.target;
-  
+
   switch(targetElement.id) {
-    case "a":
+    case "1":
       descriptionDiv.innerHTML = descriptions[0];
       break;
-    case "b":
+    case "2":
       descriptionDiv.innerHTML = descriptions[1];
       break;
-    case "c":
+    case "3":
       descriptionDiv.innerHTML = descriptions[2];
       break;
-    case "d":
+    case "4":
       descriptionDiv.innerHTML = descriptions[3];
       break;
     default:
