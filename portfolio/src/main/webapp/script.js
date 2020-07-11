8b58158774dc4580a8d4d4c3965299de6f6884ff
Shradha <shradhakhapra1899@gmail.com>
2020-07-11 22:07:43 +0530

    Change ids, TODO, JS function & comment

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,27 +1,29 @@
 /**
   * This is the main javascript file for the portfolio page. Contains a showProjectDetail method.
+
+  Copyright 2019 Google LLC
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+
   * @author: shradha-khapra
   */
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 /**
   * Shows details of a specified project.
-  * @param {HTMLElement.itemId} buttonId The ID of the button that was clicked to invoke the function.
+  * @param {void} 
   * @return {void}
   */
-function showProjectDetail(buttonId) {
+function showProjectDetail(event) {
   const descriptions =
       [
        "A Machine Learning Bot classifier used for segregation of Bots and Humans on Twitter during Indian socio-political uproar.",
@@ -31,9 +33,10 @@ function showProjectDetail(buttonId) {
       ];
 
   // Add description to the page. 
-  const descriptionDiv = document.getElementById(buttonId+"-div");
-
-  switch(buttonId) {
+  const descriptionDiv = document.createElement('div');
+  const targetElement = event.target;
+  
+  switch(targetElement.id) {
     case "a":
       descriptionDiv.innerHTML = descriptions[0];
       break;
@@ -49,5 +52,7 @@ function showProjectDetail(buttonId) {
     default:
       descriptionDiv.innerHTML = "OOPS! Something went wrong.";
   }
+  
+  targetElement.parentElement.appendChild(descriptionDiv);
 
 }
